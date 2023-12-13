e88cebc7288ff564073a07a737b459835266d493
luciamicheloud <123605664+luciamicheloud@users.noreply.github.com>
2023-12-13 02:27:47 -0300

    fffffff

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -3,12 +3,12 @@ const getAllDogs = require('../controllers/getAllDogs');
 const getDogsById = require('../controllers/getDogById');
 const getTemperaments = require('../controllers/getTemperaments');
 const postDog = require('../controllers/postDog');
-const getDogsByname = require('../controllers/getDogByname.js');
+const getDogByname = require('../controllers/getDogByname.js');
 
 const router = Router();
 
 router.get('/dogs', getAllDogs)
-router.get('/dogs/name', getDogsByname)  // ojo con el orden de las rutas
+router.get('/dogs/name', getDogByname)  // ojo con el orden de las rutas
 router.get('/dogs/:id', getDogsById)
 router.post('/dogs', postDog)
 router.get('/temperaments', getTemperaments)
